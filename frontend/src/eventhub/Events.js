b0317206a8ae39a4421f2cd4822799cafa5c72fb
erika <eelston@umass.edu>
2025-03-30 18:38:32 -0400

    Add frontend subfolders and init package.json
    Also added BaseComponent and EventHub from CS326 lecture

---
+++ frontend/src/eventhub/Events.js
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * Based on Events.js from CS326 frontend integration example tasks-v2
+ * This object contains the various event/message types used in our app
+ *********************************************************************/
+
+/**
+ * An object containing various message types for task management.
+ */
+export const Events = {
+    // example: NewTask: 'NewTask', LoadTasks: 'LoadTasks', UnStoreTasksSuccess: 'UnStoreTasksSuccess'
+
+  
+  };
+  
