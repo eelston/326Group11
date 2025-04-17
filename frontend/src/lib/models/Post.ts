c62f28092d9e2d4ff2d719d2f1db61548901e25d
Julia Farber <150543655+JuliaFarber@users.noreply.github.com>
2025-04-17 16:29:13 -0400

    Update Post.ts
    added proper data structure imports and references.

--- frontend/src/lib/models/Post.ts
+++ frontend/src/lib/models/Post.ts
@@ -1,7 +1,11 @@
+import { Tag } from "./Tag.ts"
+import { Comment } from "./Comment.ts"
+import { User } from "./User.ts"
+
 export interface Post {
-    userId: string;
-    userName: string;
-    userPronouns: string;
+    userId: string; // see: User.ts
+    userName: string; // see: User.ts
+    userPronouns: string; // see: User.ts
     
     postId: string; 
     title: string;
