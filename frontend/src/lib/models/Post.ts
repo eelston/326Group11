b64b1af424a00d6c792601cbb65c89ce441f51be
Julia Farber <150543655+JuliaFarber@users.noreply.github.com>
2025-04-17 16:18:31 -0400

    Update Post.ts
    added preliminary data structure for Post.js. Does not contain data structures for tag or comment.

--- frontend/src/lib/models/Post.ts
+++ frontend/src/lib/models/Post.ts
@@ -1,3 +1,16 @@
 export interface Post {
-    // TO BE IMPLEMENTED
+    userId: string;
+    userName: string;
+    userPronouns: string;
+    
+    postId: string; 
+    title: string;
+    tags: Tag[]; // see: Tag.js
+    description: string;
+    location: string;
+    startTime: Date; // Parsed 
+    timeStamp: Date; // Parsed
+    isExpired: boolean; 
+    
+    comments: Comment[]; // see: Comment.js
 }
