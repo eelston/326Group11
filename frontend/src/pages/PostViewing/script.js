f0c4d8093124f9dd9f14d2b7f2a73c7ec8c970b1
erika elston <eelston00@gmail.com>
2025-04-25 11:44:22 -0400

    header fix on post viewing/creation pages
    removed component css for navbar (removed any tags that already appear in main.css), added header component to post viewing

--- frontend/src/pages/PostViewing/script.js
+++ frontend/src/pages/PostViewing/script.js
@@ -1,12 +1,10 @@
 import { PostViewingComponent } from "../../components/PostViewingComponent/PostViewingComponent.js"
 import { PostRepositoryService } from "../../services/PostRepositoryService.js"
 import { mockFeed } from "../../lib/data/MockFeed.js"
-// import { NavbarComponent } from '../../components/NavbarComponent/NavbarComponent.js';
+import { NavbarComponent } from '../../components/NavbarComponent/NavbarComponent.js';
 
-// const navbarComponent = new NavbarComponent();
-// navbarComponent.render();
-// looks like there's some styling conflicts with the navbar on this page,
-// I can check which tags are messing with the navbar elements later on, not super high priority at the moment - erika
+const navbarComponent = new NavbarComponent();
+navbarComponent.render();
 
 const service = new PostRepositoryService();
 await service.initDB();
