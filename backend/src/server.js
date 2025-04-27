c3eb8e0cffe6ffba7d8dd4ba496493e399d2c161
Ashley <kangashley03@gmail.com>
2025-04-27 19:39:58 -0400

    Added backend to settings page

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,27 +3,35 @@
 // 'npm install'
 // 'node backend/src/server.js'
 // and navigate to http://localhost:3000/pages/FOLDERNAME
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import settingsRouter from './routes/settings.js';
 
 const app = express();
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url); // get file path
-const __dirname = path.dirname(__filename); // get file folder
-const url = path.join(__dirname, "../../frontend/src"); // ref: https://stackoverflow.com/a/76335925 and https://expressjs.com/en/starter/static-files.html
+const frontendDir = path.join(__dirname, "../../frontend/src");
 
-// setup middleware to serve static files from frontend directory
-app.use(express.static(url)); 
-console.log(`Serving static files from ${url}`);
-
-// setup middleware to parse incoming JSON data & add new data
 app.use(express.json());
 
-// TODO: additional setup 
+app.use(express.static(frontendDir));
+
+app.use('/api', settingsRouter);
+
+app.get('/pages/:page', (req, res) => {
+    res.sendFile(path.join(frontendDir, 'pages', req.params.page, 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something broke!' });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ frontend/src/eventhub/SettingsEvents.js
@@ -0,0 +1,12 @@
+/**
+ * An object containing event types used in settings.
+ **/
+
+export const Events = {
+    SAVE_PREFERENCES: 'SAVE_PREFERENCES',
+    SAVE_PROFILE: 'SAVE_PROFILE',
+    SAVE_ACCOUNT: 'SAVE_ACCOUNT',
+    ADD_CLASS: 'ADD_CLASS',
+    SETTINGS_ERROR: 'SETTINGS_ERROR',
+    SETTINGS_SUCCESS: 'SETTINGS_SUCCESS'
+};
