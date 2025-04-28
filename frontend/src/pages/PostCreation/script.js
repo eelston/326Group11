1da1b0c2e046acc565fbea00c8f94a6e5be7afd4
aisakov04 <aisakov@umass.edu>
2025-04-27 23:10:46 -0400

    added navbar to post creation

--- frontend/src/pages/PostCreation/script.js
+++ frontend/src/pages/PostCreation/script.js
@@ -1,4 +1,8 @@
 import { PostCreationComponent } from "../../components/PostCreationComponent/PostCreationComponent.js";
+import { NavbarComponent } from '../../components/NavbarComponent/NavbarComponent.js';
+
+const navbarComponent = new NavbarComponent();
+navbarComponent.render();
 
 const page = document.getElementById("page");
 const postCreation = new PostCreationComponent();
